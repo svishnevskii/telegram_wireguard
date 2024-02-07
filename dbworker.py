bf80613cde3a38ed6c5a57dbc2c9d3c4b603c017
Vishnevskii <s.vishnevskii@yandex.ru>
2024-02-08 00:48:49 +0300

    upd dbworker again

--- dbworker.py
+++ dbworker.py
@@ -3,9 +3,11 @@ import sqlite3
 import time
 import subprocess
 
-CONFIG = {}
-DBCONNECT = "data.sqlite"
 
+
+
+CONFIG={}
+DBCONNECT="data.sqlite"
 
 class User:
     def __init__(self):
@@ -23,7 +25,7 @@ class User:
         self.tgid = tgid
         db = await aiosqlite.connect(DBCONNECT)
         db.row_factory = sqlite3.Row
-        c = await db.execute(f"SELECT * FROM userss where tgid=?", (tgid,))
+        c = await db.execute(f"SELECT * FROM userss where tgid=?",(tgid,))
         log = await c.fetchone()
         await c.close()
         await db.close()
@@ -38,6 +40,7 @@ class User:
             self.registered = False
 
         return self
+
 
     async def PaymentInfo(self):
         db = await aiosqlite.connect(DBCONNECT)
@@ -54,14 +57,16 @@ class User:
                          (self.tgid,))
         await db.commit()
 
-    async def NewPay(self, bill_id, summ, time_to_add, mesid):
+    async def NewPay(self,bill_id,summ,time_to_add,mesid):
         pay_info = await self.PaymentInfo()
-        # print(pay_info)
+        #print(pay_info)
         if pay_info is None:
             db = await aiosqlite.connect(DBCONNECT)
             await db.execute(f"INSERT INTO payments (tgid,bill_id,amount,time_to_add,mesid) values (?,?,?,?,?)",
-                             (self.tgid, str(bill_id), summ, int(time_to_add), str(mesid)))
+                             (self.tgid, str(bill_id),summ,int(time_to_add),str(mesid)))
             await db.commit()
+
+
 
     async def GetAllPaymentsInWork(self):
         db = await aiosqlite.connect(DBCONNECT)
@@ -72,15 +77,15 @@ class User:
         await db.close()
         return log
 
-    async def Adduser(self, username, full_name):
+    async def Adduser(self,username,full_name):
         if self.registered == False:
             db = await aiosqlite.connect(DBCONNECT)
-            await db.execute(f"INSERT INTO userss (tgid,subscription,username,fullname) values (?,?,?,?)", (
-            self.tgid, str(int(time.time()) + int(CONFIG['trial_period']) * 86400), str(username), str(full_name)))
+            await db.execute(f"INSERT INTO userss (tgid,subscription,username,fullname) values (?,?,?,?)", (self.tgid,str(int(time.time())+int(CONFIG['trial_period']) * 86400),str(username),str(full_name)))
             await db.commit()
-            check = subprocess.call(f'./addusertovpn.sh {str(self.tgid)}', shell=True)
-            # print(check)
+            check=subprocess.call(f'./addusertovpn.sh {str(self.tgid)}',shell=True)
+            #print(check)
             self.registered = True
+
 
     async def GetAllUsers(self):
         db = await aiosqlite.connect(DBCONNECT)
@@ -103,7 +108,7 @@ class User:
     async def GetAllUsersWithSub(self):
         db = await aiosqlite.connect(DBCONNECT)
         db.row_factory = sqlite3.Row
-        c = await db.execute(f"SELECT * FROM userss where subscription > ?", (str(int(time.time())),))
+        c = await db.execute(f"SELECT * FROM userss where subscription > ?",(str(int(time.time())),))
         log = await c.fetchall()
         await c.close()
         await db.close()
@@ -118,15 +123,23 @@ class User:
         await db.close()
         return log
 
-    async def CheckNewNickname(self, message):
+
+
+
+    async def CheckNewNickname(self,message):
         try:
             username = "@" + str(message.from_user.username)
         except:
             username = str(message.from_user.id)
 
-        if message.from_user.full_name != self.fullname or username != self.username:
+        if message.from_user.full_name!=self.fullname or username!=self.username:
             db = await aiosqlite.connect(DBCONNECT)
             db.row_factory = sqlite3.Row
-            await db.execute(f"Update userss set username = ?, fullname = ? where id = ?",
-                             (username, message.from_user.full_name, self.id))
+            await db.execute(f"Update userss set username = ?, fullname = ? where id = ?", (username,message.from_user.full_name,self.id))
             await db.commit()
+
+
+
+
+
+
